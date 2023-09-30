9062e6ed4d8d4b065513dc29068b26d2121753f2
Olmo Zavala <olmozavala@gmail.com>
2023-09-30 16:31:19 -0400

    Added example for CLT and MCM

--- 2_CLT.py
+++ 2_CLT.py
@@ -3,16 +3,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 10 # Number of dices thrown
+n = 5# Number of dices thrown
 N = 10000 # Number of times the dice is thrown
+nbins = 6*n -(n-1)
 
 rand_values = np.random.randint(1,7,(n,N)) # Generate random values
 
-sum_rand_values = np.sum(rand_values, axis=0)/n # Sum the values of each throw
+fig, axs = plt.subplots(1,2, figsize=(8,4))
+sum_rand_values = np.sum(rand_values, axis=0) # Sum the values of each throw
 
-# %%
-plt.hist(sum_rand_values, bins=100, density=True) # Plot histogram
-plt.xlabel('Sum of dice values')
-plt.ylabel('Probability')
-plt.title(f'Central Limit Theorem. Mean: {np.mean(sum_rand_values):.2f} Variance: {np.var(sum_rand_values):.2f}')
+axs[0].hist(sum_rand_values, bins=nbins) # Plot histogram
+axs[0].set_xlabel(f'Sum of dice values (n={n})')
+axs[0].set_ylabel('Count')
+axs[0].set_title(f'Sum of distributions')
+
+sum_rand_values = np.sum(rand_values, axis=0)/n # Sum the values of each throw
+axs[1].hist(sum_rand_values, bins=nbins) # Plot histogram
+axs[1].set_xlabel(f'Mean of sum of dice values (n={n})')
+axs[1].set_ylabel('Count')
+axs[1].set_title(f'CLT. Mean: {np.mean(sum_rand_values):.2f} Variance: {np.var(sum_rand_values):.2f}')
 plt.show()
---
+++ 3_MCM.py
@@ -0,0 +1,82 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Similate MCM for solving the integral xsin(x)dx from 0 to pi
+
+# Define the function to be integrated
+def myf(x):
+    return x*np.sin(x)
+
+# Define the integral limits
+a = 0
+b = np.pi
+
+# Define the number of points to be used
+N = 100000
+
+# Generate random values
+rand_values = np.pi*np.random.rand(N)
+
+# Calculate the integral
+integral = np.mean(myf(rand_values))*(b-a)
+
+# Print the result
+print(f'The integral of xsin(x)dx from {a} to {b:.2f} is {integral:.2f}')
+# %%
+
+# --------  Error comparison -------
+
+# Compare MCM with an estimate of equispaced points
+
+N = np.logspace(2,5,100).astype(int) # Number of points to be used
+
+# Generate random values
+mcm = []
+mcm_u = []
+for n in N:
+    rand_values = np.pi*np.random.rand(n)
+    rand_values_u = np.linspace(a,b,n)
+
+    # Calculate the integral
+    mcm.append(np.abs(np.pi - np.mean(myf(rand_values))*(b-a)))
+    mcm_u.append(np.abs(np.pi -np.mean(myf(rand_values_u))*(b-a)))
+
+# plot the results
+plt.figure(figsize=(8,4))
+plt.plot(N, mcm, label='MCM')
+plt.plot(N, mcm_u, label='Uniform')
+plt.plot(N, N**(-1/2), '--', label='-1/2 Power')
+plt.xlabel('Number of points')
+plt.ylabel('Absolute error')
+# Increase the lines in the grid
+plt.legend()
+plt.grid()
+# Make y axis logarithmic
+plt.yscale('log')
+plt.xscale('log')
+plt.show()
+
+
+# %% --------  Area on the integral comparison -------
+# Here we compute the area inside a circle of radius 1 using MCM
+
+# Define the function to be integrated
+def myfcircle(x,y):
+    return np.sqrt(x**2+y**2)<1
+
+# Define the integral limits
+a = -1
+b = 1
+
+# Define the number of points to be used
+N = 1000
+
+# Generate random values
+rand_values_x = -1 + 2*np.random.rand(N)
+rand_values_y = -1 + 2*np.random.rand(N)
+
+integral = np.mean(myfcircle(rand_values_x,rand_values_y))*(b-a)*2
+
+# Print the result
+print(f'The area of the circle is {integral:.2f} vs {np.pi:.2f}')
